cbdc86fde2196ec53829bdf3d4f81a1419a814c0
rajmund88 <lukaszrajda@gmail.com>
2018-05-27 20:53:17 +0200

    Initial Commit

---
+++ GeoLib.Proxies/IMessageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+
+namespace GeoLib.Client.Contracts
+{
+    [ServiceContract(Namespace = "http://www.pluralsight.com/test")]
+    public interface IMessageService
+    {
+        [OperationContract]
+        void ShowMessage(string message);
+    }
+}
